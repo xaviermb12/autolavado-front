3032eada8aa34c6d85d4456c877dcb8c28a50655
EmilianoP2310 <emiliano@anunciatedigital.com>
2020-06-06 13:21:32 -0500

    ejemplo store paquetes

---
+++ store/estados.js
@@ -0,0 +1,26 @@
+export const state = () => ({
+  estados: []
+})
+
+export const actions = {
+  async getAll({ commit, state }) {
+    try {
+      if (state.estados.length > 0) return
+
+      const { data } = await this.$axios.get('/estados')
+
+      commit('SET_ESTADOS', data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
+
+export const mutations = {
+  SET_ESTADOS(state, estados) {
+    state.estados = estados
+  },
+  CLEAN_DATA(state) {
+    state.estados = []
+  }
+}
